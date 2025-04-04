c671023ecf106135bbbf372195e46e0ecc56f34a
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-04-04 11:27:50 +0200

    Initial Commit

---
+++ MySwiftTestingProjectTwo/MySwiftTestingProjectTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftTestingProjectTwoApp.swift
+//  MySwiftTestingProjectTwo
+//
+//  Created by Manyuchi, Carrington C on 2025/04/04.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftTestingProjectTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MySwiftTestingProjectTwoTests/MySwiftTestingProjectTwoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftTestingProjectTwoTests.swift
+//  MySwiftTestingProjectTwoTests
+//
+//  Created by Manyuchi, Carrington C on 2025/04/04.
+//
+
+import Testing
+@testable import MySwiftTestingProjectTwo
+
+struct MySwiftTestingProjectTwoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
