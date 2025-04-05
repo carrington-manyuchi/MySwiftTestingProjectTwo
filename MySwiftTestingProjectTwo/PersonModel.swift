38080c52918443f6e7cc20a5e7bcb909eb0464f6
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-04-05 14:56:05 +0200

    two

---
+++ MySwiftTestingProjectTwo/PersonModel.swift
@@ -0,0 +1,14 @@
+//
+//  PersonModel.swift
+//  MySwiftTestingProjectTwo
+//
+//  Created by Manyuchi, Carrington C on 2025/04/04.
+//
+
+import Foundation
+
+
+struct PersonModel {
+    var name: String
+    var age: Int
+}
--- MySwiftTestingProjectTwoTests/MySwiftTestingProjectTwoTests.swift
+++ MySwiftTestingProjectTwoTests/MySwiftTestingProjectTwoTests.swift
@@ -8,10 +8,18 @@
 import Testing
 @testable import MySwiftTestingProjectTwo
 
+
 struct MySwiftTestingProjectTwoTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
+    @Test func verifyPeoplesAgesAreTheSame() {
+        let person1 = PersonModel(name: "Carrington", age: 30)
+        let person2 = PersonModel(name: "Josephine", age: 30)
+        
+        #expect(person1.age == person2.age)
+        
+    }
 }
